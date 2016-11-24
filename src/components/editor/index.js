9274d3786e7acc55b62796b3859b7d181b7eb037
820514 <820514@nd>
2016-11-24 17:45:01 +0800

    update

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Modal } from 'antd'
+import Upload from 'components/upload'
+
 import 'kindeditor'
 import 'kindeditor/themes/default/default.css'
+import './theme/styles'
 
 export default class extends React.Component {
   constructor() {
@@ -28,19 +32,25 @@ export default class extends React.Component {
     height: '500'
   }
 
+  state = {
+    // 图片上传框是否可视
+    imgUploadVisible: false
+  }
+
   componentDidMount() {
     const that = this
     const {name, afterChange, height} = this.props
     const items = [
       'source',
-      'image', 'fullscreen', 'undo', 'redo', 'justifyleft', 'justifycenter', 'justifyright',
+      'fullscreen', 'undo', 'redo', 'justifyleft', 'justifycenter', 'justifyright',
       'justifyfull', 'indent', 'outdent', 'subscript',
       'superscript', 'link',
       'fontname', 'formatblock',
       'fontsize',
       'forecolor', 'hilitecolor', 'bold',
       'italic', 'underline', 'strikethrough', 'lineheight', 'removeformat', 'picture',
-      'hr'
+      'hr',
+      'imgUpload'
     ]
     const options = {
       width: '100%',
@@ -60,9 +70,49 @@ export default class extends React.Component {
     }, 100)
 
     this.editor = KindEditor.create(this.refs.content, {...options})
+
+    // 自定义插入图片插件
+    KindEditor.lang({
+      'imgUpload': '插入图片'
+    })
+
+    KindEditor.plugin('imgUpload', function (K) {
+      this.clickToolbar('imgUpload', function () {
+        that.setState({
+          imgUploadVisible: true
+        })
+        //that.insertHtml('<strong>测试内容</strong>')
+      })
+    })
   }
 
   render() {
-    return <textarea ref="content" />
+    return <div>
+      <textarea ref="content" />
+      <Modal title="插入图片" visible={this.state.imgUploadVisible} okText="插入" cancelText="取消"
+        onOk={this._handleOk} onCancel={this._handleCancel}>
+        <Upload />
+      </Modal>
+    </div>
+  }
+
+  /**
+   * 确定插入
+   */
+  _handleOk = () => {
+    this.setState({
+      imgUploadVisible: false
+    })
+
+    this.editor.insertHtml(333)
+  }
+
+  /**
+   * 取消
+   */
+  _handleCancel = () => {
+    this.setState({
+      imgUploadVisible: false
+    })
   }
 }
