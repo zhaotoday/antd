0431862dfbba7ab9b9c2457182b4d2c4e8f43fba
zhaotoday <729234283@qq.com>
2016-12-20 23:12:36 +0800

    update

---
+++ src/redux/actions/commodities.js
@@ -0,0 +1,73 @@
+import actionTypes from '../consts/commodities'
+import Model from '../models/commodities'
+import createAction from 'redux-actions/lib/createAction'
+
+/**
+ * 获取商品列表
+ */
+export const getCommodity = createAction(
+  actionTypes.GET_COMMODITY,
+  (options = {}) => {
+    return new Model()
+      .addPath('{commodity_id}')
+      .replace({
+        'commodity_id': options['commodity_id']
+      })
+      .GET()
+  }
+)
+
+/**
+ * 获取商品列表
+ */
+export const getCommodities = createAction(
+  actionTypes.GET_COMMODITIES,
+  (options = {}) => {
+    return new Model().GET({
+      params: options.params
+    })
+  }
+)
+
+/**
+ * 新增商品
+ */
+export const postCommodity = createAction(
+  actionTypes.POST_COMMODITY,
+  (options = {}) => {
+    return new Model()
+      .POST({
+        data: options.data
+      })
+  }
+)
+
+/**
+ * 编辑商品
+ */
+export const patchCommodity = createAction(
+  actionTypes.PATCH_COMMODITY,
+  (options = {}) => {
+    return new Model()
+      .addPath('{commodity_id}')
+      .replace({
+        'commodity_id': options['commodity_id']
+      })
+      .PATCH({
+        data: options.data
+      })
+  }
+)
+
+/**
+ * 删除商品
+ */
+export const deleteCommodity = createAction(
+  actionTypes.DELETE_COMMODITY,
+  (options = {}) => {
+    return new Model()
+      .DELETE({
+        params: options.params
+      })
+  }
+)
---
+++ src/redux/actions/jobs.js
@@ -0,0 +1,73 @@
+import actionTypes from '../consts/jobs'
+import Model from '../models/jobs'
+import createAction from 'redux-actions/lib/createAction'
+
+/**
+ * 获取岗位列表
+ */
+export const getJob = createAction(
+  actionTypes.GET_JOB,
+  (options = {}) => {
+    return new Model()
+      .addPath('{job_id}')
+      .replace({
+        'job_id': options['job_id']
+      })
+      .GET()
+  }
+)
+
+/**
+ * 获取岗位列表
+ */
+export const getJobs = createAction(
+  actionTypes.GET_JOBS,
+  (options = {}) => {
+    return new Model().GET({
+      params: options.params
+    })
+  }
+)
+
+/**
+ * 新增岗位
+ */
+export const postJob = createAction(
+  actionTypes.POST_JOB,
+  (options = {}) => {
+    return new Model()
+      .POST({
+        data: options.data
+      })
+  }
+)
+
+/**
+ * 编辑岗位
+ */
+export const patchJob = createAction(
+  actionTypes.PATCH_JOB,
+  (options = {}) => {
+    return new Model()
+      .addPath('{job_id}')
+      .replace({
+        'job_id': options['job_id']
+      })
+      .PATCH({
+        data: options.data
+      })
+  }
+)
+
+/**
+ * 删除岗位
+ */
+export const deleteJob = createAction(
+  actionTypes.DELETE_JOB,
+  (options = {}) => {
+    return new Model()
+      .DELETE({
+        params: options.params
+      })
+  }
+)
